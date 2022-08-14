f703b7982ac28eb12dd26edc000b778e6e8af184
Kotik <k-work31@yandex.ru>
2022-08-14 15:53:02 +0300

    lesson4. Добавлены доработки для мока репозитория

--- app/src/main/java/com/example/githubclientapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubclientapp/ui/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.githubclientapp.ui
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.githubclientapp.R
-import com.example.githubclientapp.app
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
